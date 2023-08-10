a21c1c0bea263be9ab1eb692f1b21473b157c204
ShradhaK9 <shradha.kumavat@cumminscollege.in>
2023-08-10 19:09:12 +0530

    Done

--- backend/BankingApp/src/main/java/com/bankingapp/controller/TransactionController.java
+++ backend/BankingApp/src/main/java/com/bankingapp/controller/TransactionController.java
@@ -1,5 +1,24 @@
 package com.bankingapp.controller;
 
-public class TransactionController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.bankingapp.models.Transaction;
+import com.bankingapp.service.TransactionService;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class TransactionController 
+{
+	@Autowired
+	TransactionService tService;
+	@PostMapping("/save/transaction")
+	
+	public Transaction saveTransaction(@RequestBody Transaction trans)
+	{
+		return tService.saveTransaction(trans);
+	}
 }
