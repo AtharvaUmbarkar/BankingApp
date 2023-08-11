cc9fa6bf279fa5808094b33aa38ce3556e321a37
ShradhaK9 <shradha.kumavat@cumminscollege.in>
2023-08-11 11:18:17 +0530

    Done

---
+++ frontend/src/Component/Registration.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react"
+
+function Registration(){
+
+    return(
+            <div>
+                
+                    <h1>Welcome to Banking app</h1>
+            </div>
+    )
+}
+
+export default Registration;
