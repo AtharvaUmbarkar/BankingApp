7a9fbf1f6e10ce04667a4877e32e996d93c3369c
ShradhaK9 <shradhakumavat911@gmail.com>
2023-08-14 20:46:36 +0530

    Done

--- frontend/src/Component/User/OnlineBankingRegistration.js
+++ frontend/src/Component/User/OnlineBankingRegistration.js
@@ -1,8 +1,74 @@
+
 import React from 'react'
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
+
 
 const OnlineBankingRegistration = () => {
+
+  const [inputs, setInputs] = useState({});
+  const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = async (event) => {
+
+  }
+
   return (
-    <div>OnlineBankingRegistration</div>
+    
+    <div className="flex flex-col w-2/5 mt-8 self-center">
+      <h2 className="text-2xl font-semibold mt-4 mb-2 w-full border-b-2 border-blue-500 pb-2">Internet Banking</h2>
+      <form onSubmit={handleSubmit} className=''>
+
+        <div className=''>
+
+          <label className="text-lg my-2">Account Number:
+            <input
+              type="text"
+              name="account_no"
+              value={inputs.account_no || ""}
+              onChange={handleChange}
+              className="border border-slate-500 focus-within:border-blue-500 text-lg p-1 mt-1"
+            />
+          </label>
+          <label className="text-lg my-2">Set Transaction Password:
+            <input
+              type="password"
+              name="transaction_password"
+              value={inputs.transaction_password || ""}
+              onChange={handleChange}
+              className="border border-slate-500 focus-within:border-blue-500 text-lg p-1 mt-1"
+            />
+          </label>
+          <label className="text-lg my-2">Confirm Transaction Password:
+            <input
+              type="password"
+              name="confirm_transaction_password"
+              value={inputs.confirm_transaction_password || ""}
+              onChange={handleChange}
+              className="border border-slate-500 focus-within:border-blue-500 text-lg p-1 mt-1"
+            />
+          </label>
+          <label className="text-lg my-2">Enter OTP:
+            <input
+              type="otp"
+              name="otp"
+              value={inputs.otp || ""}
+              onChange={handleChange}
+              className="border border-slate-500 focus-within:border-blue-500 text-lg p-1 mt-1"
+            />
+          </label>
+        </div>
+        <input type="submit" value="Submit" className="self-center p-2 uppercase bg-blue-800 text-white my-4"/>
+      </form>
+    </div>
   )
 }
 
