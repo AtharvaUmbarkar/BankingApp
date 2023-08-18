81377d4f3af7ab317b7e76d6c8c0325d5b19feed
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-08-18 14:11:59 +0530

    Moved file to match file structure

--- frontend/src/Component/User/UserAccounts.js
+++ frontend/src/Component/User/UserAccounts.js
@@ -6,7 +6,7 @@ const BASE_URL = "http://localhost:8090/fetchAccounts/";
 const USER_NAME = JSON.parse(sessionStorage.getItem('user'));
 const TABLE_HEAD = ["Accounts"];
 
-const Account = () => {
+const UserAccounts = () => {
   const [accountsList, setAccounts] = useState([]);
 
   const fetchAccounts = () => {
@@ -26,9 +26,9 @@ const Account = () => {
       <div className='w-1/4 my-8'>
         {accountsList.map((account, i) => {
           return (
-            <div className='w-full p-4 my-2 bg-slate-200 shadow flex flex-col rounded'>
+            <div className='w-full p-4 my-2 bg-slate-100 shadow-md flex flex-col rounded'>
               <p><span className='font-semibold'>Account Number: </span><span>{account}</span></p>
-              <Link to='/account' className='self-end bg-blue-600 text-white py-1 px-2 rounded mt-8'>Enter Account</Link>
+              <Link to={'/account/' + account} className='self-end bg-blue-600 text-white py-1 px-2 rounded mt-8'>Enter Account</Link>
             </div>
           )
         })}
@@ -37,4 +37,4 @@ const Account = () => {
   )
 }
 
-export default Account
+export default UserAccounts
