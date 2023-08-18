d9a9b68b85079e5d80ffb9ecabdb9fc6f1c92861
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-08-18 14:29:04 +0530

    Fixed Account Navbar Links

--- frontend/src/Component/Account/AccountDetails.js
+++ frontend/src/Component/Account/AccountDetails.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const AccountDetails = () => {
+  const { accountNumber } = useParams();
   return (
-    <div>AccountDetails</div>
+    <div>{accountNumber}</div>
   )
 }
 
