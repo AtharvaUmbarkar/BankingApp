b85f3e6cb21de046cd000828fcc06382da6c0ef0
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-08-21 01:57:42 +0530

    Completed Forgot Password Page

--- frontend/src/Utilities/context/userContext.js
+++ frontend/src/Utilities/context/userContext.js
@@ -5,22 +5,34 @@ export const UserContext = React.createContext()
 export const UserContextProvider = (props) => {
     const [username, setUsername] = useState(
         sessionStorage.getItem("user") ?
-        JSON.parse(sessionStorage.getItem("user")).username : undefined
-        )
+            JSON.parse(sessionStorage.getItem("user")).username : undefined
+    )
 
-    const setUsernameInContext = (uname) => {
-        sessionStorage.setItem("user", JSON.stringify({username: uname}))
-        setUsername(uname)
+    const [user, setUser] = useState(
+        sessionStorage.getItem("user") ?
+            JSON.parse(sessionStorage.getItem("user")).username : undefined
+    )
+
+    const setUsernameInContext = (user) => {
+        sessionStorage.setItem("user", JSON.stringify({ username: user.userName }))
+        setUsername(user.userName)
+        setUser(user)
+    }
+
+    const removeUser = () => {
+        setUser(undefined);
+        setUsername(undefined);
+        sessionStorage.removeItem('user');
     }
 
     useEffect(() => {
-        if(sessionStorage.getItem("user")){
+        if (sessionStorage.getItem("user")) {
             setUsername(JSON.parse(sessionStorage.getItem("user")).username)
         }
     }, [])
 
     return (
-        <UserContext.Provider value={{username, setUsernameInContext}}>
+        <UserContext.Provider value={{ username, setUsernameInContext, user, removeUser }}>
             {props.children}
         </UserContext.Provider>
     )
