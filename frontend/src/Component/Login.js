b0cfa346a5d11c50b893a881fc2aa4cb82f863cd
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-08-21 23:35:21 +0530

    Added Admin Login in Login Page

--- frontend/src/Component/Login.js
+++ frontend/src/Component/Login.js
@@ -13,42 +13,49 @@ const condition = (authUser) => authUser
 export default withAuthorization(condition, HOME)(() => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [admin, setAdmin] = useState(false);
   const navigate = useNavigate()
-  const { setUsernameInContext }  = useContext(UserContext)
+  const { setUsernameInContext } = useContext(UserContext)
 
 
   const handleSubmit = async (e) => {
-      e.preventDefault();
-      if(username && password){
-          const response = await loginUser({username, password})
-          if(response){
-              if(response.data.aadhaarNumber){
-                  const toastId = toast.loading("Logging you in...")
-                  setTimeout(() => {
-                      setUsernameInContext(response.data)
-                      toast.dismiss(toastId)
-                      navigate("/")
-                  }, 1000)
-              } else{
-                  toast.error("Login failed!")
-              }
+    e.preventDefault();
+    if (username && password) {
+      try {
+        const response = await loginUser({ username, password }, admin)
+        console.log(response);
+        if (response) {
+          if (response.data.mobileNumber) {
+            const toastId = toast.loading("Logging you in...")
+            setTimeout(() => {
+              setUsernameInContext(response.data)
+              toast.dismiss(toastId)
+              if (admin) navigate("/")
+              else navigate("/user")
+            }, 1000)
+          } else {
+            toast.error("Login failed!")
           }
+        }
+      } catch (error) {
+        console.log(error.response.data);
       }
+    }
   }
 
 
   return (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Link to="/" className="-m-1.5 p-1.5 text-center">
-            <span className="sr-only">Banking App</span>
-            <p className='font-serif font-bold text-3xl text-indigo-700'>BA</p>
-            {/* <img
+        <Link to="/" className="-m-1.5 p-1.5 text-center">
+          <span className="sr-only">Banking App</span>
+          <p className='font-serif font-bold text-3xl text-indigo-700'>BA</p>
+          {/* <img
               className="h-8 w-auto"
               src="https://ogden_images.s3.amazonaws.com/www.nashuatelegraph.com/images/2018/12/29000134/1024px-Wells_Fargo_Bank.svg_-840x840.png"
               alt=""
             /> */}
-          </Link>
+        </Link>
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Log in to your account
         </h2>
@@ -70,7 +77,7 @@ export default withAuthorization(condition, HOME)(() => {
                 className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                />
+              />
             </div>
           </div>
 
@@ -95,6 +102,17 @@ export default withAuthorization(condition, HOME)(() => {
                 className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mt-4 flex flex-row items-center justify-center">
+              <label htmlFor="admin" className="mr-6 whitespace-nowrap text-sm">Sign In as Admin?</label>
+              <input
+                id="admin"
+                name="admin"
+                type="checkbox"
+                className="w-4 h-4"
+                value={admin}
+                onChange={(e) => setAdmin(!admin)}
               />
             </div>
           </div>
