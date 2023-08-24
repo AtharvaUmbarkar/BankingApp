9f1d9209e4bee4783b8552213e7f923405b0f520
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-08-24 14:42:04 +0530

    Added elegance to the app, now app beautiful uwu

--- frontend/src/Component/User/UserProfile.js
+++ frontend/src/Component/User/UserProfile.js
@@ -9,84 +9,84 @@ const UserProfile = () => {
     <div className='w-full p-8'>
       <div className='w-2/5 flex flex-col mx-auto items-center'>
         <h1 className='font-semibold text-2xl mb-8'>User Details</h1>
-        <div className='w-full shadow-md rounded bg-slate-200 p-2'>
+        <div className='w-full flex flex-col divide-y space-y-2 divide-indigo-200 shadow-md rounded p-2'>
           {/* {Object.keys(user).map((key, i) => {
             return (
-              <div key={key} className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+              <div key={key} className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
                 <span className='w-1/2 font-semibold'>{key}:</span>
                 <span className='flex-grow'>{user[key]}</span>
               </div>
             )
           })} */}
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Customer ID:</span>
             <span className='flex-grow'>{user.customerId}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Username:</span>
             <span className='flex-grow'>{user.userName}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Title:</span>
             <span className='flex-grow'>{user.title}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>First Name:</span>
             <span className='flex-grow'>{user.firstName}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Middle Name:</span>
             <span className='flex-grow'>{user.middleName}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Last Name:</span>
             <span className='flex-grow'>{user.lastName}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Father's Name:</span>
             <span className='flex-grow'>{user.fatherName}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Email ID:</span>
             <span className='flex-grow'>{user.emailId}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Mobile No.:</span>
             <span className='flex-grow'>{user.mobileNumber}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Aadhaar No.:</span>
             <span className='flex-grow'>{user.aadhaarNumber}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Date of Birth:</span>
             <span className='flex-grow'>{user.dateOfBirth}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Residential Add. Line 1:</span>
             <span className='flex-grow'>{user.tempAddressLine1}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Residential Add. Line 2:</span>
             <span className='flex-grow'>{user.tempAddressLine2}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Permanent Add. Line 1:</span>
             <span className='flex-grow'>{user.permAddressLine1}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Permanent Add. Line 2:</span>
             <span className='flex-grow'>{user.permAddressLine2}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Occupation:</span>
             <span className='flex-grow'>{user.occupation}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Source of Income:</span>
             <span className='flex-grow'>{user.sourceOfIncome}</span>
           </div>
-          <div className='w-full flex flex-row items-center p-1 px-2 my-0.5 rounded'>
+          <div className='w-full flex flex-row items-center p-1 pt-2 px-2 rounded'>
             <span className='w-1/2 font-semibold'>Gross annual Income:</span>
             <span className='flex-grow'>{user.grossAnnualIncome}</span>
           </div>
